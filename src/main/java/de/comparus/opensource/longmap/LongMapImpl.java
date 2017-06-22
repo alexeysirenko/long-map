959feac91bfafcdea42a340bb66d737a62166d83
Alexey Sirenko <drunk.pacifist@gmail.com>
2017-06-22 16:44:24 +0300

    Add documentation

--- src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -10,42 +10,66 @@ import java.util.List;
  */
 public class LongMapImpl<V> implements LongMap<V> {
 
+    /**
+     * Size of a hash table.
+     */
     private int HASH_TABLE_SIZE = 4096;
 
-    private List<List<Cons<V>>> hashTable;
+    /**
+     * Hash table. Contains list of items with the same hashes (collisions).
+     */
+    private List<List<Pair<V>>> hashTable;
 
+    /**
+     * Creates a map instance.
+     */
     public LongMapImpl() {
         init();
     }
 
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map previously contained a mapping for the key, the old value is replaced.
+     *
+     * @param key key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
+     * @return the previous value associated with key, or null if there was no mapping for key
+     */
     public V put(long key, V value) {
         int hash = hash(key);
-        List<Cons<V>> items = hashTable.get(hash);
-        if (items == null) {
-            items = new LinkedList<>();
-            hashTable.set(hash, items);
+        List<Pair<V>> pairs = hashTable.get(hash);
+        if (pairs == null) {
+            pairs = new LinkedList<>();
+            hashTable.set(hash, pairs);
         }
-        boolean exists = false;
-        for (Cons<V> item: items) {
-            if (item.key == key) {
-                exists = true;
-                item.value = value;
+        V previousValue = null;
+        for (Pair<V> pair : pairs) {
+            if (pair.key == key) {
+                previousValue = pair.value;
+                pair.value = value;
             }
         }
-        if (!exists) {
-            items.add(new Cons<>(key, value));
+        if (previousValue == null) {
+            pairs.add(new Pair<>(key, value));
         }
-        return value;
+        return previousValue;
     }
 
+    /**
+     * Returns the value to which the specified key is mapped, or null if this map
+     * contains no mapping for the key.
+     *
+     * @param key the key whose associated value is to be returned
+     * @return the value to which the specified key is mapped, or null if this map contains no mapping for the key
+     */
     public V get(long key) {
         V result = null;
         int hash = hash(key);
-        List<Cons<V>> items = hashTable.get(hash);
-        if (items != null) {
-            for (Cons<V> item: items) {
-                if (item.key == key) {
-                    result = item.value;
+        List<Pair<V>> pairs = hashTable.get(hash);
+        if (pairs != null) {
+            for (Pair<V> pair : pairs) {
+                if (pair.key == key) {
+                    result = pair.value;
                     break;
                 }
             }
@@ -53,31 +77,42 @@ public class LongMapImpl<V> implements LongMap<V> {
         return result;
     }
 
+    /**
+     * Removes the mapping for the specified key from this map if present.
+     *
+     * @param key key whose mapping is to be removed from the map
+     * @return the previous value associated with key, or null if there was no mapping for key
+     */
     public V remove(long key) {
         V result = null;
         int hash = hash(key);
-        List<Cons<V>> items = hashTable.get(hash);
-        if (items != null) {
-            Iterator<Cons<V>> iterator = items.iterator();
+        List<Pair<V>> pairs = hashTable.get(hash);
+        if (pairs != null) {
+            Iterator<Pair<V>> iterator = pairs.iterator();
             while (iterator.hasNext()) {
-                Cons<V> item = iterator.next();
-                if (item.key == key) {
-                    result = item.value;
+                Pair<V> pair = iterator.next();
+                if (pair.key == key) {
+                    result = pair.value;
                     iterator.remove();
                     break;
                 }
             }
-            if (items.isEmpty()) {
+            if (pairs.isEmpty()) {
                 hashTable.set(hash, null);
             }
         }
         return result;
     }
 
+    /**
+     * Returns true if this map contains no key-value mappings.
+     *
+     * @return true if this map contains no key-value mappings
+     */
     public boolean isEmpty() {
         boolean isEmpty = true;
-        for (List<Cons<V>> items: hashTable) {
-            if (items != null && !items.isEmpty()) {
+        for (List<Pair<V>> pairs : hashTable) {
+            if (pairs != null && !pairs.isEmpty()) {
                 isEmpty = false;
                 break;
             }
@@ -85,13 +120,19 @@ public class LongMapImpl<V> implements LongMap<V> {
         return isEmpty;
     }
 
+    /**
+     * Returns true if this map contains a mapping for the specified key.
+     *
+     * @param key key whose presence in this map is to be tested
+     * @return true if this map contains a mapping for the specified key
+     */
     public boolean containsKey(long key) {
         boolean containsKey = false;
         int hash = hash(key);
-        List<Cons<V>> items = hashTable.get(hash);
-        if (items != null) {
-            for (Cons<V> item: items) {
-                if (item.key == key) {
+        List<Pair<V>> pairs = hashTable.get(hash);
+        if (pairs != null) {
+            for (Pair<V> pair : pairs) {
+                if (pair.key == key) {
                     containsKey = true;
                     break;
                 }
@@ -100,12 +141,18 @@ public class LongMapImpl<V> implements LongMap<V> {
         return containsKey;
     }
 
+    /**
+     * Returns true if this map maps one or more keys to the specified value.
+     *
+     * @param value value whose presence in this map is to be tested
+     * @return true if this map maps one or more keys to the specified value
+     */
     public boolean containsValue(V value) {
         boolean containsValue = false;
-        for (List<Cons<V>> items: hashTable) {
-            if (items != null) {
-                for (Cons<V> item: items) {
-                    if (item.value.equals(value)) {
+        for (List<Pair<V>> pairs : hashTable) {
+            if (pairs != null) {
+                for (Pair<V> pair : pairs) {
+                    if (pair.value.equals(value)) {
                         containsValue = true;
                         break;
                     }
@@ -115,12 +162,17 @@ public class LongMapImpl<V> implements LongMap<V> {
         return containsValue;
     }
 
+    /**
+     * Returns all the keys contained in this map.
+     *
+     * @return array of keys
+     */
     public long[] keys() {
         List<Long> keys = new ArrayList<>();
-        for (List<Cons<V>> items: hashTable) {
-            if (items != null) {
-                for (Cons<V> item: items) {
-                    keys.add(item.key);
+        for (List<Pair<V>> pairs : hashTable) {
+            if (pairs != null) {
+                for (Pair<V> pair : pairs) {
+                    keys.add(pair.key);
                 }
             }
         }
@@ -131,16 +183,22 @@ public class LongMapImpl<V> implements LongMap<V> {
         return result;
     }
 
+    /**
+     * Returns all the values contained in this map.
+     *
+     * @return array of values
+     */
     public V[] values() {
         List<V> values = new ArrayList<>();
-        for (List<Cons<V>> items: hashTable) {
-            if (items != null) {
-                for (Cons<V> item: items) {
-                    values.add(item.value);
+        for (List<Pair<V>> pairs : hashTable) {
+            if (pairs != null) {
+                for (Pair<V> pair : pairs) {
+                    values.add(pair.value);
                 }
             }
         }
 
+        // Looks like that there is no better way to instantiate a generic array
         @SuppressWarnings("unchecked")
         V[] result = (V[]) new Object[values.size()];
         for (int i = 0; i < values.size(); i++) {
@@ -149,35 +207,59 @@ public class LongMapImpl<V> implements LongMap<V> {
         return result;
     }
 
+    /**
+     * Returns the number of key-value mappings in this map.
+     *
+     * @return the number of key-value mappings in this map
+     */
     public long size() {
         long size = 0;
-        for (List<Cons<V>> items: hashTable) {
-            if (items != null) {
-                size = size + items.size();
+        for (List<Pair<V>> pairs : hashTable) {
+            if (pairs != null) {
+                size = size + pairs.size();
             }
         }
         return size;
     }
 
+    /**
+     * Removes all of the mappings from this map.
+     */
     public void clear() {
         init();
     }
 
+    public String toString() {
+        return "[" + hashTable.toString() + "]";
+    }
+
+    /**
+     * Returns calculated hash of a key.
+     *
+     * @param key a key to be hashed
+     * @return hash value
+     */
     private int hash(long key) {
         return (int) (key % HASH_TABLE_SIZE);
     }
 
+    /**
+     * Initializes a hash table
+     */
     private void init() {
         hashTable = new ArrayList<>(HASH_TABLE_SIZE);
     }
 
-    private class Cons<V> {
+    /**
+     * Class to hold key-value pairs.
+     */
+    private class Pair<V> {
 
         public final long key;
 
         public V value;
 
-        public Cons(long key, V value) {
+        public Pair(long key, V value) {
             this.key = key;
             this.value = value;
         }
